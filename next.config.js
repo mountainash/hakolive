51cbcc0f996497cda951b09bac623c5155c6aaa8
mountainash <mountainash@rmwpublishing.net>
2021-02-18 09:00:03 +0100

    Sitemap.xml generator added
    From https://github.com/vercel/next.js/tree/canary/examples/with-sitemap

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ const path = require('path')
 module.exports = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
+	},
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			require('./scripts/generate-sitemap')
+		}
+
+		return config
 	}
 }
